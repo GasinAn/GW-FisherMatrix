6975735b2cf768085150830185052245c552625f
GasinAn <Gasin185@163.com>
2021-10-28 18:56:49 +0800

    fix many mistakes: minus symbol in F(h)_{,k} and f_0 = 10 -> 70, and recalculate prod_ij

--- I_q.py
+++ I_q.py
@@ -8,6 +8,6 @@ def I_7(f):
     return f**(-7/3)/(f**(-4)+2+2*f**2)
 
 
-print(quad(I_7, 1/7, (6**(3/2)*m.pi*2.8*(M_sun*G/c**3).value*10)**(-1)))
-# print(quad(I_7, 1/7, (6**(3/2)*m.pi*11.4*(M_sun*G/c**3).value*10)**(-1)))
-# print(quad(I_7, 1/7, (6**(3/2)*m.pi*20*(M_sun*G/c**3).value*10)**(-1)))
+print(quad(I_7, 1/7, (6**(3/2)*m.pi*2.8*(M_sun*G/c**3).value*70)**(-1))[0])
+# print(quad(I_7, 1/7, (6**(3/2)*m.pi*11.4*(M_sun*G/c**3).value*70)**(-1))[0])
+# print(quad(I_7, 1/7, (6**(3/2)*m.pi*20*(M_sun*G/c**3).value*70)**(-1))[0])
--- JiaChen_Fisher.py
+++ JiaChen_Fisher.py
@@ -21,13 +21,13 @@ S = frac(1,5)*S_0*((f_0/f)**4+2+2*(f/f_0)**2)
 h_F_1 = (1)*h_F
 h_F_2 = (2*pi*I*(f/f_0))*h_F
 h_F_3 = (-I)*h_F
-h_F_4 = (frac(5,128)*I*(pi*M*f)**(-frac(5,3))
+h_F_4 = (-frac(5,128)*I*(pi*M*f)**(-frac(5,3))
          *(1+A_4*v**2-B_4*v**3+C_4*v**4))*h_F
-h_F_5 = (frac(1,96)*I*(pi*M*f)**(-frac(5,3))
+h_F_5 = (-frac(1,96)*I*(pi*M*f)**(-frac(5,3))
          *(A_5*v**2-B_5*v**3+C_5*v**4))*h_F
 h_F_6 = (frac(3,32)*I*(pi*M*f)**(-frac(2,3))
          *eta**(-frac(3,5)))*h_F
-h_F_7 = (frac(15,64)*I*(pi*M*f)**(-frac(1,3))
+h_F_7 = (-frac(15,64)*I*(pi*M*f)**(-frac(1,3))
          *eta**(-frac(4,5)))*h_F
 
 h_F_4 = h_F_4.subs(A_4, frac(4,3)*(frac(743,336)+frac(11,4)*eta))
@@ -41,19 +41,19 @@ h_F_5 = h_F_5.subs(C_5, 18*epsilon*(frac(3058673,1016064)-frac(5429,4032)*eta
 for n in range(1,8):
     exec(f'h_F_{n}=h_F_{n}.subs(v,(pi*M/eta**frac(3,5)*f)**frac(1,3))')
 
-eta_value = 0.25
-M_value = 2.8*(M_sun*G/c**3).value
-# eta_value = 0.1077254539858418
-# M_value = 11.4*(M_sun*G/c**3).value
-# eta_value = 0.25
-# M_value = 20*(M_sun*G/c**3).value
+eta_value = (1.4*1.4)/(1.4+1.4)**2
+M_value = (1.4*1.4)*(M_sun*G/c**3).value
+# eta_value = (1.4*10)/(1.4+10)**2
+# M_value = (1.4*10)*(M_sun*G/c**3).value
+# eta_value = (10*10)/(10+10)**2
+# M_value = (10*10)*(M_sun*G/c**3).value
 
 epsilon_value = 1
 # epsilon_value = 0
 # epsilon_value = -1
 
-S = S.subs(f_0, 10)
-# SNR = SNR.subs(f_0, 10)
+S = S.subs(f_0, 70)
+# SNR = SNR.subs(f_0, 70)
 for n in range(1,8):
     exec(f'h_F_{n}=h_F_{n}.subs(f_0,70)')
     exec(f'h_F_{n}=h_F_{n}.subs(M,eta_value**(3/5)*M_value)')    
@@ -62,14 +62,14 @@ for n in range(1,8):
     exec(f'h_F_{n}=h_F_{n}.subs(sigma,0)')
     exec(f'h_F_{n}=h_F_{n}.subs(epsilon,epsilon_value)')
 
-I_7 = 0.28390566003167744
-# I_7 = 0.2839048939137505
-# I_7 = 0.28390063547499106
+I_7 = 0.28390096292585254
+# I_7 = 0.2834082747325775
+# I_7 = 0.2807955590004223
 for i in range(1,8):
     for j in range(i,8):
         exec(f'prod = (conj(h_F_{i})*h_F_{j}+h_F_{i}*conj(h_F_{j}))/S')
         prod = prod.subs(h_F*conj(h_F), (A*f**(-frac(7,6)))**2)
-        prod = prod.subs(A**2*S_0**(-1), SNR/(20*10**(-4/3)*I_7))
+        prod = prod.subs(A**2*S_0**(-1), SNR/(20*70**(-4/3)*I_7))
         prod = prod.subs(SNR, 10**2)
         print(f'i={i}, j={j}')
         print(prod)
