465b30a08e428713517094c6d6566f3b3775c546
GasinAn <Gasin185@163.com>
2022-01-15 15:59:00 +0800

    add h_alpha (also A_alpha and chi_alpha)

--- Monochromatic_Sources.py
+++ Monochromatic_Sources.py
@@ -6,30 +6,30 @@ T = sqrt(R**3/(1.3271244e+20/(4*pi**2)))
 varphi_0 = 0
 ln_A = 0
 
-def A_alpha(A_p, A_t, F_p_alpha, F_t_alpha):
+def func_A_alpha(A_p, A_t, F_p_alpha, F_t_alpha):
     return sqrt((A_p*F_p_alpha)**2+(A_t*F_t_alpha)**2)
 
-def chi_alpha(t, f_0, varphi_p_alpha, varphi_D):
+def func_chi_alpha(t, f_0, varphi_p_alpha, varphi_D):
     return 2*pi*f_0*t+varphi_0+varphi_p_alpha+varphi_D
 
-def varphi_p_alpha(A_p, A_t, F_p_alpha, F_t_alpha):
+def func_varphi_p_alpha(A_p, A_t, F_p_alpha, F_t_alpha):
     return arctan(-(A_t*F_t_alpha)/(A_p*F_p_alpha))
 
-def varphi_D(f_0, bar_theta_S, bar_phi, bar_phi_S):
+def func_varphi_D(f_0, bar_theta_S, bar_phi, bar_phi_S):
     return 2*pi*f_0/c*R*sin(bar_theta_S)*cos(bar_phi-bar_phi_S)
 
-def Lz(bar_theta_L, bar_phi, bar_phi_L):
+def func_Lz(bar_theta_L, bar_phi, bar_phi_L):
     return ((sqrt(1)/2)*cos(bar_theta_L)
            -(sqrt(3)/2)*sin(bar_theta_L)*cos(bar_phi-bar_phi_L))
 
-def Ln(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S):
+def func_Ln(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S):
     return (cos(bar_theta_L)*cos(bar_theta_S)
            +sin(bar_theta_L)*sin(bar_theta_S)*cos(bar_phi_L-bar_phi_S))
 
-def zn(theta_S):
+def func_zn(theta_S):
     return cos(theta_S)
 
-def nLz(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S, bar_phi):
+def func_nLz(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S, bar_phi):
     return ((1/2)*sin(bar_theta_L)*sin(bar_theta_S)*sin(bar_phi_L-bar_phi_S)
             -(sqrt(3)/2)*cos(bar_phi)
                         *(cos(bar_theta_L)*sin(bar_theta_S)*sin(bar_phi_S)
@@ -38,45 +38,45 @@ def nLz(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S, bar_phi):
                         *(cos(bar_theta_S)*sin(bar_theta_L)*cos(bar_phi_S)
                          -cos(bar_theta_L)*sin(bar_theta_S)*cos(bar_phi_L)))
 
-def A_p(Ln):
+def func_A_p(Ln):
     return 2*exp(ln_A)*(1+Ln**2)
 
-def A_t(Ln):
+def func_A_t(Ln):
     return -2*exp(ln_A)*Ln
 
-def F_p_I(theta_S, phi_S, psi_S):
+def func_F_p_I(theta_S, phi_S, psi_S):
     return (cos(2*phi_S)*cos(2*psi_S)*(1+cos(theta_S)**2)/2
            -sin(2*phi_S)*sin(2*psi_S)*cos(theta_S))
 
-def F_t_I(theta_S, phi_S, psi_S):
+def func_F_t_I(theta_S, phi_S, psi_S):
     return (cos(2*phi_S)*sin(2*psi_S)*(1+cos(theta_S)**2)/2
            -sin(2*phi_S)*cos(2*psi_S)*cos(theta_S))
 
-def F_p_II(theta_S, phi_S, psi_S):
+def func_F_p_II(theta_S, phi_S, psi_S):
     return (sin(2*phi_S)*cos(2*psi_S)*(1+cos(theta_S)**2)/2
            +cos(2*phi_S)*sin(2*psi_S)*cos(theta_S))
 
-def F_t_II(theta_S, phi_S, psi_S):
+def func_F_t_II(theta_S, phi_S, psi_S):
     return (sin(2*phi_S)*sin(2*psi_S)*(1+cos(theta_S)**2)/2
            +cos(2*phi_S)*cos(2*psi_S)*cos(theta_S))
 
-def theta_S(bar_theta_S, bar_phi, bar_phi_S):
+def func_theta_S(bar_theta_S, bar_phi, bar_phi_S):
     return arccos(
            ((sqrt(1)/2)*cos(bar_theta_S)
            -(sqrt(3)/2)*sin(bar_theta_S)*cos(bar_phi-bar_phi_S)))
 
-def phi_S(t, bar_theta_S, bar_phi, bar_phi_S):
+def func_phi_S(t, bar_theta_S, bar_phi, bar_phi_S):
     alpha_0 = 0
     return alpha_0+2*pi*t/T+arctan(
         (sin(bar_theta_S)*cos(bar_phi-bar_phi_S)-sqrt(3)*cos(bar_theta_S))
         /(2*sin(bar_theta_S)*cos(bar_phi-bar_phi_S)))
 
-def psi_S(Lz, Ln, zn, nLz):
+def func_psi_S(Lz, Ln, zn, nLz):
     return arctan((Lz-Ln*zn)/nLz)
 
-def phi(t):
-    phi_0 = 0
-    return phi_0+2*pi*t/T
+def func_bar_phi(t):
+    bar_phi_0 = 0
+    return bar_phi_0+2*pi*t/T
 
 def S_n(f):
     alpha = 10**(-22.79)*(f/1e-3)**(-7/3)
@@ -90,3 +90,41 @@ def S_n(f):
     else:
         S_n_co = 10**(-46.850)*f**(-2.6)
     return S_n_in+S_n_co
+
+def h_I(t, f_0, bar_theta_S, bar_phi_S, bar_theta_L, bar_phi_L):
+    bar_phi = func_bar_phi(t)
+    theta_S = func_theta_S(bar_theta_S, bar_phi, bar_phi_S)
+    Lz = func_Lz(bar_theta_L, bar_phi, bar_phi_L)
+    Ln = func_Ln(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S)
+    zn = func_zn(theta_S)
+    nLz = func_nLz(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S, bar_phi)
+    phi_S = func_phi_S(t, bar_theta_S, bar_phi, bar_phi_S)
+    psi_S = func_psi_S(Lz, Ln, zn, nLz)
+    A_p = func_A_p(Ln)
+    A_t = func_A_t(Ln)
+    F_p_I = func_F_p_I(theta_S, phi_S, psi_S)
+    F_t_I = func_F_t_I(theta_S, phi_S, psi_S)
+    A_I = func_A_alpha(A_p, A_t, F_p_I, F_t_I)
+    varphi_p_I = func_varphi_p_alpha(A_p, A_t, F_p_I, F_t_I)
+    varphi_D = func_varphi_D(f_0, bar_theta_S, bar_phi, bar_phi_S)
+    chi_I = func_chi_alpha(t, f_0, varphi_p_I, varphi_D)
+    return (sqrt(3)/2)*A_I*cos(chi_I), A_I, chi_I
+
+def h_II(t, f_0, bar_theta_S, bar_phi_S, bar_theta_L, bar_phi_L):
+    bar_phi = func_bar_phi(t)
+    theta_S = func_theta_S(bar_theta_S, bar_phi, bar_phi_S)
+    Lz = func_Lz(bar_theta_L, bar_phi, bar_phi_L)
+    Ln = func_Ln(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S)
+    zn = func_zn(theta_S)
+    nLz = func_nLz(bar_theta_L, bar_theta_S, bar_phi_L, bar_phi_S, bar_phi)
+    phi_S = func_phi_S(t, bar_theta_S, bar_phi, bar_phi_S)
+    psi_S = func_psi_S(Lz, Ln, zn, nLz)
+    A_p = func_A_p(Ln)
+    A_t = func_A_t(Ln)
+    F_p_II = func_F_p_II(theta_S, phi_S, psi_S)
+    F_t_II = func_F_t_II(theta_S, phi_S, psi_S)
+    A_II = func_A_alpha(A_p, A_t, F_p_II, F_t_II)
+    varphi_p_II = func_varphi_p_alpha(A_p, A_t, F_p_II, F_t_II)
+    varphi_D = func_varphi_D(f_0, bar_theta_S, bar_phi, bar_phi_S)
+    chi_II = func_chi_alpha(t, f_0, varphi_p_II, varphi_D)
+    return (sqrt(3)/2)*A_II*cos(chi_II), A_II, chi_II
